0365a3496621077d6d4c9d29c6ff0e755fb7a953
Pramit Bhar <pramitbhar26@gmail.com>
2023-04-04 21:00:37 +0530

    try

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar';
-
+import Hero from '../components/hero/Hero';
+import HomeImg from '../assets/img01';
 const Home = () => {
   return (
     <>
     <Navbar />
+    
+    
+    <Hero />
     </>
   )
 }
